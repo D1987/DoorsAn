da5854cd38272b631f9b0f8e221b717b6d579b73
dzmitry.zimin <dzmitry.zimin@sqil.by>
2018-01-17 18:21:15 +0300

    I've added nav bar for main page

--- DoorsAngular/src/app/app.module.ts
+++ DoorsAngular/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent
+,
+    NavComponent
 ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
